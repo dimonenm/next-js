7634dd93b7f159d20a843cf3d9227b5f48bc35a2
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-09-27 17:44:35 +0300

    w27.09.2022

--- top-app/components/Sort/Sort.tsx
+++ top-app/components/Sort/Sort.tsx
@@ -6,16 +6,18 @@ import cn from 'classnames';
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span className={cn({
+      <button className={cn({
         [styles.active]: sort === SortEnum.Raiting 
-      })} onClick={()=> setSort(SortEnum.Raiting)} >
+      })}
+        onClick={() => setSort(SortEnum.Raiting)}
+      >
         <SortIcon className={ styles.sortIcon} />По рейтингу
-      </span>
-      <span className={cn({
+      </button>
+      <button className={cn({
         [styles.active]: sort === SortEnum.Price 
       })} onClick={()=> setSort(SortEnum.Price)} >
         <SortIcon className={styles.sortIcon} />По цене
-      </span>
+      </button>
     </div>
   )
 }
